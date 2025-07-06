035df6e86fe757c50d71fa6198dcdd97da286df1
Arulraj Gopal <arulrajgopal@outlook.com>
2025-07-06 15:45:50 +0530

    create statement

---
+++ delta_load_multi_source_table/createStatement.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS orders_stage (
+    order_id INTEGER PRIMARY KEY,      
+    customer_id INT NOT NULL,        
+    order_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders_items_stage (
+    order_id INT NOT NULL,             
+    product_id INT NOT NULL,   
+    unit_price DECIMAL(10, 2) NOT NULL,      
+    qty INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INTEGER PRIMARY KEY,      
+    customer_id INT NOT NULL,        
+    order_date DATE NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id INTEGER PRIMARY KEY, 
+    order_id INT NOT NULL,             
+    product_id INT NOT NULL,   
+    unit_price DECIMAL(10, 2) NOT NULL,      
+    qty INT NOT NULL,      
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,           
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS order_details (
+    order_item_id INT NOT NULL,             
+    order_id INT NOT NULL,             
+    product_id INT NOT NULL, 
+    customer_id INT NOT NULL,        
+    order_date DATE NOT NULL,  
+    unit_price DECIMAL(10, 2) NOT NULL,      
+    qty INT NOT NULL,      
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,           
+    PRIMARY KEY (order_id, order_item_id),  
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (order_item_id) REFERENCES order_items(order_item_id) ON DELETE CASCADE
+);
