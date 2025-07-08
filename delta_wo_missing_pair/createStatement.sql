f362e692d3b4233c430579ded49be5eeaf9e58b6
arulrajgopal <arulrajgopal@outlook.com>
2025-07-08 17:19:24 +0000

    update

--- delta_wo_missing_pair/createStatement.sql
+++ delta_wo_missing_pair/createStatement.sql
@@ -107,13 +107,13 @@ BEGIN
 
     INSERT INTO order_details (order_id, customer_id, order_date, order_item_id, product_id,unit_price,qty)
     SELECT 
-    A.order_id,
-    A.customer_id,
-    A.order_date,
-    B.order_item_id,
-    B.product_id,
-    B.unit_price,
-    B.qty
+        A.order_id,
+        A.customer_id,
+        A.order_date,
+        B.order_item_id,
+        B.product_id,
+        B.unit_price,
+        B.qty
     FROM orders A 
     join order_items B
     on A.order_id = B.order_id;
