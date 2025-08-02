cd0efff04d55561c45ccf3873f2238c6b47a8a5f
arulraj <arulrajgopal@outlook.com>
2025-08-02 07:06:54 +0000

    update

--- orphan_records_handling/main.py
+++ orphan_records_handling/main.py
@@ -13,31 +13,31 @@ sql_path = f"{BASE_DIR}/createStatement.sql"
 utils.run_sql_script(sql_path)
 
 
-# batch_no = sys.argv[1]
-# storage_account = "arulrajgopalshare"
-# container = "kaniniwitharul"
+batch_no = sys.argv[1]
+storage_account = "arulrajgopalshare"
+container = "kaniniwitharul"
 
 
 
-# #reading the data from blob storage
-# orders_url = f"https://{storage_account}.blob.core.windows.net/{container}/orphan_records_handling/{batch_no}/orders.csv"
-# order_items_url = f"https://{storage_account}.blob.core.windows.net/{container}/orphan_records_handling/{batch_no}/order_items.csv"
+#reading the data from blob storage
+orders_url = f"https://{storage_account}.blob.core.windows.net/{container}/orphan_records_handling/{batch_no}/orders.csv"
+order_items_url = f"https://{storage_account}.blob.core.windows.net/{container}/orphan_records_handling/{batch_no}/order_items.csv"
 
-# orders_df = pd.read_csv(orders_url)
-# order_items_df = pd.read_csv(order_items_url)
-
-
-# orders_df['order_date'] = pd.to_datetime(orders_df['order_date'], format='%m/%d/%Y').dt.date
+orders_df = pd.read_csv(orders_url)
+order_items_df = pd.read_csv(order_items_url)
 
 
-# #loading into sql stage tables
-# utils.load_to_sql(order_items_df,"order_items_stage")
-# utils.load_to_sql(orders_df,"orders_stage")
+orders_df['order_date'] = pd.to_datetime(orders_df['order_date'], format='%m/%d/%Y').dt.date
 
 
-# #executing stored procedures
-# utils.run_stored_proc("load_orders")
-# utils.run_stored_proc("load_order_items")
+#loading into sql stage tables
+utils.load_to_sql(order_items_df,"order_items_stage")
+utils.load_to_sql(orders_df,"orders_stage")
+
+
+#executing stored procedures
+utils.run_stored_proc("load_orders")
+utils.run_stored_proc("load_order_items")
 # # utils.run_stored_proc("load_order_details")
 
 
