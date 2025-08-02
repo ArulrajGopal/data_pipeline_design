527dfe5beec0bef0d475bf1ab0a682f47915c1fe
arulraj <arulrajgopal@outlook.com>
2025-08-02 08:07:21 +0000

    create statement updated

--- orphan_records_handling/createStatement.sql
+++ orphan_records_handling/createStatement.sql
@@ -1,8 +1,8 @@
--- drop table order_details;
--- drop table order_items;
--- drop table orders;
--- drop table order_items_stage;
--- drop table orders_stage;
+drop table order_details;
+drop table order_items;
+drop table orders;
+drop table order_items_stage;
+drop table orders_stage;
 
 
 CREATE TABLE IF NOT EXISTS orders_stage (
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS orders_stage (
 );
 
 CREATE TABLE IF NOT EXISTS order_items_stage (
+    order_id INT NOT NULL, 
     order_item_id INT NOT NULL,
-    order_id INT NOT NULL,             
     product_id INT NOT NULL,   
     unit_price DECIMAL(10, 2) NOT NULL,      
     qty INT NOT NULL
@@ -27,17 +27,18 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
-    order_item_id INTEGER PRIMARY KEY, 
-    order_id INT NOT NULL,             
+    order_id INT NOT NULL, 
+    order_item_id INT NOT NULL,        
     product_id INT NOT NULL,   
     unit_price DECIMAL(10, 2) NOT NULL,      
     qty INT NOT NULL,      
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (order_id, order_item_id)
 );
 
 CREATE TABLE IF NOT EXISTS order_details (
-    order_item_id INT NOT NULL,             
-    order_id INT NOT NULL,             
+    order_id INT NOT NULL, 
+    order_item_id INT NOT NULL,           
     product_id INT NOT NULL, 
     customer_id INT NOT NULL,        
     order_date DATE NOT NULL,  
@@ -75,26 +76,33 @@ CREATE OR REPLACE PROCEDURE load_order_items()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    INSERT INTO order_items (order_item_id, order_id, product_id, unit_price, qty)
+    INSERT INTO order_items (order_id, order_item_id, product_id, unit_price, qty)
     SELECT 
-    order_item_id,
     order_id,
+    order_item_id,
     product_id,
     unit_price,
     qty
     FROM order_items_stage
-    ON CONFLICT (order_item_id) DO UPDATE
+    ON CONFLICT (order_id, order_item_id) DO UPDATE
     SET
-        order_id = EXCLUDED.order_id,
         product_id = EXCLUDED.product_id,
         unit_price = EXCLUDED.unit_price,
         qty = EXCLUDED.qty,
         updated_at = NOW();
 
-
     RAISE NOTICE 'order_items table loaded successfully.';
 END;
 $$;
+
+
+    -- order_id INT NOT NULL, 
+    -- order_item_id INT NOT NULL,           
+    -- product_id INT NOT NULL, 
+    -- customer_id INT NOT NULL,        
+    -- order_date DATE NOT NULL,  
+    -- unit_price DECIMAL(10, 2) NOT NULL,      
+    -- qty INT NOT NULL,   
 
 
 
@@ -102,15 +110,15 @@ CREATE OR REPLACE PROCEDURE load_order_details()
 LANGUAGE plpgsql
 AS $$
     BEGIN
-        INSERT INTO order_details (order_item_id, order_id, product_id, customer_id, order_date, unit_price, qty)
+        INSERT INTO order_details (order_id, order_item_id, product_id, customer_id, order_date, unit_price, qty)
         WITH 
         order_join_order_items AS (
-            SELECT B.order_item_id, B.order_id, B.product_id, A.customer_id, A.order_date, B.unit_price, B.qty 
+            SELECT B.order_id, B.order_item_id, B.product_id, A.customer_id, A.order_date, B.unit_price, B.qty 
             FROM orders_stage A 
             JOIN order_items B ON A.order_id = B.order_id
         ),
         order_items_join_orders AS (
-            SELECT A.order_item_id, A.order_id, A.product_id, B.customer_id, B.order_date, A.unit_price, A.qty
+            SELECT  A.order_id, A.order_item_id, A.product_id, B.customer_id, B.order_date, A.unit_price, A.qty
             FROM order_items_stage A 
             LEFT JOIN orders_stage B ON A.order_id = B.order_id
             JOIN orders C ON A.order_id = C.order_id
@@ -130,6 +138,8 @@ AS $$
     RAISE NOTICE 'order_details table loaded successfully.';
 END;
 $$;
+
+
 
 
 
